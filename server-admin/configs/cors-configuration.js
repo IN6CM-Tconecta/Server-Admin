28bea5b30e56b4b2977b39fd8d560b31509a8ccc
serv-drv <sderosa-2023220@kinal.edu.gt>
2026-07-27 20:45:33 -0600

    fix: corregir errores tipograficos en corsOptions

--- server-admin/configs/cors-configuration.js
+++ server-admin/configs/cors-configuration.js
@@ -1,7 +1,7 @@
 const corsOptions = {
     origin: true,
-    credenciales: true,
-    mehtods: "GET,POST,PUT,DELETE",
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization"
 }
  
